076295d0cf0c21e683e79dc874da7eae0802cddf
Juan Giraldo <jgiraldo@tvmarkets.com>
2023-12-18 21:14:47 -0500

    dando diseno

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,3 +1,12 @@
-<template><h1 class="header__name">hola mundo</h1></template>
+<template>
+  <div class="header">
+    <div class="header__name">
+      <img class="name__logo" src="../../public/vue.png" alt="logo de vue" />
+      <h1 class="name__major">
+        APRENDE <span class="name__major--vue">VUE.JS</span>
+      </h1>
+    </div>
+  </div>
+</template>
 
 <script setup></script>
